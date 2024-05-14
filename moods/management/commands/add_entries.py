7beef99d8bb2b405f1fa8da0a984131945f6f5ab
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-05-14 10:33:15 +0200

    feat(moods): display activities on entry list

--- moods/management/commands/add_entries.py
+++ moods/management/commands/add_entries.py
@@ -2,7 +2,7 @@ import random
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
 from faker import Faker
-from moods.models import Mood, Entry
+from moods.models import Activity, Mood, Entry
 
 fake = Faker()
 
@@ -34,20 +34,25 @@ class Command(BaseCommand):
                 user = get_user_model().objects.get(username=username)
 
                 moods = Mood.objects.all().filter(user=user)
+                activities = Activity.objects.all().filter(user=user)
 
-                entries = [
-                    Entry(
+                for _ in range(n):
+                    mood = random.choice(moods)
+
+                    entry = Entry.objects.create(
                         user=user,
-                        mood=random.choice(moods),
+                        mood=mood,
                         note_title=fake.sentence(),
                         note_body=fake.paragraph(),
                         date=fake.date(),
                         time=fake.time(),
                     )
-                    for _ in range(n)
-                ]
 
-                Entry.objects.bulk_create(entries)
+                    random_activities = random.sample(
+                        list(activities), random.randint(0, 3)
+                    )
+                    entry.activities.add(*random_activities)
+                    entry.save()
 
                 self.stdout.write(
                     self.style.SUCCESS(f"Successfully added entries to {user}")
